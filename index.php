425ea4a21b4b4df6774c0da01c271560a1865fcb
Ujjal Bhattacharya <75716127+henry-n2@users.noreply.github.com>
2024-04-08 01:46:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "layouts/header.php"; ?>
+<div class="container">
+
+<center><h2 style="color:black; margin-top:20%">Cybersecurity Capstone Project by Ujjal Bhattacharya</h2></center>
+<center><a href="http://henry-n2.great-site.net/"><img src="images/logo.png" width="180" height="60"></a></center>
+</div>
+
+</body>
+</html>
